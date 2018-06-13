96b6e18d1595c0da21a6a4aacfc32fdc68baaf4c
Rakshith1992 <32623125+Rakshith1992@users.noreply.github.com>
2018-06-13 20:17:16 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		for(int i=1;i<10;i++){
-			System.out.println("hello world"+i);
+			System.out.println("hello ld"+i);
 		}
 	}
 }
