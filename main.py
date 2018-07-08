a2982ee03382eceb9192816e95c678916a81ece8
Rohit Gollarahalli <rohit.gs28@gmail.com>
2018-07-07 23:49:15 -0300

    Updated main.py with call to Business layer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import *
 
 from werkzeug.utils import secure_filename
-import Databaselayer
-from Databaselayer import *
+import Businesslayer
+from Businesslayer import *
 
 app = Flask(__name__)
 app.secret_key = 'random string'
@@ -17,8 +17,8 @@ def root():
         return render_template('home.html',  loggedIn=loggedIn, firstName=firstName)
     else:
         loggedIn = True
-        login = Databaselayer.Databaselayer_LoginClass()
-        loggedIn, firstName = login.getLoginDetails_DBL(session['email'])
+        loginclassdetails = Businesslayer.Businesslayer_LoginClass()
+        loggedIn, firstName = loginclassdetails.getLoginDetails_BSL(session['email'])
         return render_template("Profile2.html", loggedIn=loggedIn, firstName=firstName )
 
 
@@ -28,18 +28,18 @@ def profileHome():
         return redirect(url_for('root'))
     else:
         loggedIn = True
-        login = Databaselayer.Databaselayer_LoginClass()
-        loggedIn, firstName = login.getLoginDetails_DBL(session['email'])
+        loginclassdetails = Businesslayer.Businesslayer_LoginClass()
+        loggedIn, firstName = loginclassdetails.getLoginDetails_BSL(session['email'])
     return render_template("Profile2.html", loggedIn=loggedIn, firstName=firstName, )
 
 @app.route("/account/profile/edit")
 def editProfile():
     if 'email' not in session:
         return redirect(url_for('root'))
-    login_object = Databaselayer.Databaselayer_LoginClass()
-    loggedIn, firstName = login_object.getLoginDetails_DBL(session['email'])
-    fetchuserdata = Databaselayer.Databaselayer_FetchUserData()
-    profileData = fetchuserdata.getProfileData_DBL(session['email'])
+    loginclassdetails = Businesslayer.Businesslayer_LoginClass()
+    loggedIn, firstName = loginclassdetails.getLoginDetails_BSL(session['email'])
+    fetchuserdata = Businesslayer.Businesslayer_FetchUserData()
+    profileData = fetchuserdata.getProfileData_BSL(session['email'])
     return render_template("editProfile.html", profileData=profileData, loggedIn=loggedIn, firstName=firstName )
 
 @app.route("/account/profile/changePassword", methods=["GET", "POST"])
@@ -51,8 +51,8 @@ def changePassword():
         oldPassword = hashlib.md5(oldPassword.encode()).hexdigest()
         newPassword = request.form['newpassword']
         newPassword = hashlib.md5(newPassword.encode()).hexdigest()
-        changemypassword = Databaselayer.Databaselayer_ChangeMyPassword()
-        msg = changemypassword.changeMyProfilePassword_DBL(session['email'],oldPassword,newPassword)
+        changemypassword = Businesslayer.Businesslayer_ChangeMyPassword()
+        msg = changemypassword.changeMyProfilePassword_BSL(session['email'],oldPassword,newPassword)
         return render_template("changePassword.html", msg=msg)
     else:
         return render_template("changePassword.html")
@@ -70,8 +70,8 @@ def updateProfile():
         state = request.form['state']
         country = request.form['country']
         phone = request.form['phone']
-        updatemyprofile = Databaselayer.Databaselayer_UpdateMyProfile()
-        msg = updatemyprofile.updateMyProfileMethod_DBL(email,firstName,lastName,address1,address2,zipcode,city,state,country,phone)
+        updatemyprofile = Businesslayer.Businesslayer_UpdateMyProfile()
+        msg = updatemyprofile.updateMyProfileMethod_BSL(email,firstName,lastName,address1,address2,zipcode,city,state,country,phone)
         return redirect(url_for('editProfile'))
 
 @app.route("/loginForm")
@@ -86,8 +86,8 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        checkifuservalid = Databaselayer.Databaselayer_CheckIfUserValid()
-        value  = checkifuservalid.isValid_DBL(email, password)
+        checkifuservalid = Businesslayer.Businesslayer_CheckIfUserValid()
+        value  = checkifuservalid.isValid_BSL(email, password)
         if value == True:
             session['email'] = email
             return redirect(url_for('root'))
@@ -118,8 +118,8 @@ def register():
         state = request.form['state']
         country = request.form['country']
         phone = request.form['phone']
-        insertuser = Databaselayer.Databaselayer_InsertUser()
-        msg = insertuser.insertNewUser_DBL(password,email,firstName,lastName,address1,address2,zipcode,city,state,country,phone)
+        insertuser = Businesslayer.Businesslayer_InsertUser()
+        msg = insertuser.insertNewUser_BSL(password,email,firstName,lastName,address1,address2,zipcode,city,state,country,phone)
         return render_template("login.html", error=msg)
 
 
