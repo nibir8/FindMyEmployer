2e0d6ba32991927ba27bdefeaee671497d41fa2d
rohit <rohit.gs28@gmail.com>
2018-07-24 17:31:52 -0300

    Adding mockdata

---
+++ Tests/MockData.py
@@ -0,0 +1,16 @@
+
+Emailid = ['rohit.gs28@gmail.com','nibir.ks2@gmail.com','a.a@gmail.com','mark.cuban@gmail.com']
+
+Firstname  = ['Rohit','Nibir','Mark','Chaz']
+
+Lastname = ['Gollarahalli','Mukherjee','Cuban','kirkpatrik']
+
+Password = ['password','password1','password2','password3']
+
+Address1 = ['1333,South park street','1333,Morris street','2134,Bolevard drive','Suncast road']
+
+City = ['Halifax','Toronto','Montreal','Michigan']
+
+Province = ['Nova Scotia','Ontario','Qubec','Alberta']
+
+Country = ['Canada','Canada','USA','Canada']
